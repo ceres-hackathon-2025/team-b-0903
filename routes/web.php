72ca1aefea1b9d6e97b3c75aa4d4886ae9b188b9
山河翔悟 <showsan2727oneok@gmail.com>
2025-09-04 11:25:47 +0900

    ModelとControllerの枠組み作成

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 投稿(Post)のルーティング
+Route::prefix('posts')->group(function () {
+    // 投稿一覧
+    Route::get('/posts', 'App\\Http\\Controllers\\PostController@index')->name('posts.index');
+    // 投稿作成フォーム
+    Route::get('/create', 'App\\Http\\Controllers\\PostController@create')->name('posts.create');
+    // 投稿保存
+    Route::post('/create', 'App\\Http\\Controllers\\PostController@store')->name('posts.store');
+    // 投稿詳細
+    Route::get('/posts/{post}', 'App\\Http\\Controllers\\PostController@show')->name('posts.show');
+    // 投稿編集フォーム
+    Route::get('/posts/{post}/edit', 'App\\Http\\Controllers\\PostController@edit')->name('posts.edit');
+    // 投稿更新
+    Route::put('/posts/{post}', 'App\\Http\\Controllers\\PostController@update')->name('posts.update');
+    // 投稿削除
+    Route::delete('/posts/{post}', 'App\\Http\\Controllers\\PostController@destroy')->name('posts.destroy');
+});
