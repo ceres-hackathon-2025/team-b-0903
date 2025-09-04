4c5520cf5689cea05ec0c88ad6e7ee42093d551a
山河翔悟 <showsan2727oneok@gmail.com>
2025-09-04 15:48:29 +0900

    PlaceControllerの作成

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PlaceController extends Controller
 {
-    //
+    // 指定したplaceの投稿数を返す
+    public function postCount($place_id)
+    {
+        $count = Post::where('place_id', $place_id)->count();
+        return response()->json(['post_count' => $count]);
+    }
+
+    // 全投稿のおすすめ度の平均を返す
+    public function recommendAverage()
+    {
+        $average = Post::avg('recommend');
+        return response()->json(['recommend_average' => $average]);
+    }
 }
