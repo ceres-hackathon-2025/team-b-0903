3c01322677dca383d3bd1afe40fd709cca2e4757
Yuta-N-18 <r202304726py@jindai.jp>
2025-09-04 13:32:40 +0900

    テスト用画像の追加、web.phpの改変、プロトタイプの作成

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 投稿(Post)のルーティング
-Route::prefix('posts')->group(function () {
-    // 投稿一覧
-    Route::get('/posts', 'App\\Http\\Controllers\\PostController@index')->name('posts.index');
-    // 投稿作成フォーム
-    Route::get('/create', 'App\\Http\\Controllers\\PostController@create')->name('posts.create');
-    // 投稿保存
-    Route::post('/create', 'App\\Http\\Controllers\\PostController@store')->name('posts.store');
-    // 投稿詳細
-    Route::get('/posts/{post}', 'App\\Http\\Controllers\\PostController@show')->name('posts.show');
-    // 投稿編集フォーム
-    Route::get('/posts/{post}/edit', 'App\\Http\\Controllers\\PostController@edit')->name('posts.edit');
-    // 投稿更新
-    Route::put('/posts/{post}', 'App\\Http\\Controllers\\PostController@update')->name('posts.update');
-    // 投稿削除
-    Route::delete('/posts/{post}', 'App\\Http\\Controllers\\PostController@destroy')->name('posts.destroy');
+Route::get('/places', function () {
+    return view('places');
 });
+
+// // 投稿(Post)のルーティング
+// Route::prefix('posts')->group(function () {
+//     // 場所一覧
+//     Route::get('/places', 'App\\Http\\Controllers\\PlaceController@index')->name('places.index');
+//     // 投稿一覧
+//     Route::get('/posts', 'App\\Http\\Controllers\\PostController@index')->name('posts.index');
+//     // 投稿作成フォーム
+//     Route::get('/create', 'App\\Http\\Controllers\\PostController@create')->name('posts.create');
+//     // 投稿保存
+//     Route::post('/create', 'App\\Http\\Controllers\\PostController@store')->name('posts.store');
+//     // 投稿詳細
+//     Route::get('/posts/{post}', 'App\\Http\\Controllers\\PostController@show')->name('posts.show');
+//     // 投稿編集フォーム
+//     Route::get('/posts/{post}/edit', 'App\\Http\\Controllers\\PostController@edit')->name('posts.edit');
+//     // 投稿更新
+//     Route::put('/posts/{post}', 'App\\Http\\Controllers\\PostController@update')->name('posts.update');
+//     // 投稿削除
+//     Route::delete('/posts/{post}', 'App\\Http\\Controllers\\PostController@destroy')->name('posts.destroy');
+// });
+
