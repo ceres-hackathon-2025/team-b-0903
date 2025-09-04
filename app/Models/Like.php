d250fb69a274a52b872598c98cd2361e8f314587
山河翔悟 <showsan2727oneok@gmail.com>
2025-09-04 13:47:12 +0900

    Modelを作成

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    protected $table = 'likes';
+    protected $fillable = [
+        'post_id',
+        'user_id',
+    ];
+
+    // リレーション例: Post
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    // リレーション例: User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Place.php
+++ app/Models/Place.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Place extends Model
 {
-    //
+    protected $table = 'places';
+    protected $fillable = [
+        'name',
+        'prefectures_id',
+        'recommend_average',
+    ];
+
+    // リレーション例: Prefecture
+    public function prefecture()
+    {
+        return $this->belongsTo(Prefecture::class, 'prefectures_id');
+    }
 }
